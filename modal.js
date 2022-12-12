0a97cd331dbecd17ea6c0b59139cb49697e3dbcb
Rennan G Macarini <74021916+Rennangmacarini@users.noreply.github.com>
2022-12-12 17:34:24 -0300

    Update modal.js

--- modal.js
+++ modal.js
@@ -103,6 +103,191 @@ modalIconNine.addEventListener("click", function(){
     modalContentNine.classList.add("hide")
 })
 
+const modalButtonTen = document.querySelector(".button-10")
+const modalContentTen = document.querySelector(".teste-10")
+const modalIconTen = document.querySelector(".icone-10")
+modalButtonTen.addEventListener("click", function(){
+    modalContentTen.classList.remove("hide")
+})
+modalIconTen.addEventListener("click", function(){
+    modalContentTen.classList.add("hide")
+})
+
+const modalButtonEleven = document.querySelector(".button-11")
+const modalContentEleven = document.querySelector(".teste-11")
+const modalIconEleven = document.querySelector(".icone-11")
+modalButtonEleven.addEventListener("click", function(){
+    modalContentEleven.classList.remove("hide")
+})
+modalIconEleven.addEventListener("click", function(){
+    modalContentEleven.classList.add("hide")
+})
+
+const modalButtonTwelve = document.querySelector(".button-12")
+const modalContentTwelve = document.querySelector(".teste-12")
+const modalIconTwelve = document.querySelector(".icone-12")
+modalButtonTwelve.addEventListener("click", function(){
+    modalContentTwelve.classList.remove("hide")
+})
+modalIconTwelve.addEventListener("click", function(){
+    modalContentTwelve.classList.add("hide")
+})
+
+const modalButtonThirteen = document.querySelector(".button-13")
+const modalContentThirteen = document.querySelector(".teste-13")
+const modalIconThirteen = document.querySelector(".icone-13")
+modalButtonThirteen.addEventListener("click", function(){
+    modalContentThirteen.classList.remove("hide")
+})
+modalIconThirteen.addEventListener("click", function(){
+    modalContentThirteen.classList.add("hide")
+})
+
+const modalButtonFourteen = document.querySelector(".button-14")
+const modalContentFourteen = document.querySelector(".teste-14")
+const modalIconFourteen = document.querySelector(".icone-14")
+modalButtonFourteen.addEventListener("click", function(){
+    modalContentFourteen.classList.remove("hide")
+})
+modalIconFourteen.addEventListener("click", function(){
+    modalContentFourteen.classList.add("hide")
+})
+
+
+const modalButtonFifteen = document.querySelector(".button-15")
+const modalContentFifteen = document.querySelector(".teste-15")
+const modalIconFifteen = document.querySelector(".icone-15")
+modalButtonFifteen.addEventListener("click", function(){
+    modalContentFifteen.classList.remove("hide")
+})
+modalIconFifteen.addEventListener("click", function(){
+    modalContentFifteen.classList.add("hide")
+})
+
+
+const modalButtonSixteen = document.querySelector(".button-16")
+const modalContentSixteen = document.querySelector(".teste-16")
+const modalIconSixteen = document.querySelector(".icone-16")
+modalButtonSixteen.addEventListener("click", function(){
+    modalContentSixteen.classList.remove("hide")
+})
+modalIconSixteen.addEventListener("click", function(){
+    modalContentSixteen.classList.add("hide")
+})
+
+const modalButtonSeventeen = document.querySelector(".button-17")
+const modalContentSeventeen = document.querySelector(".teste-17")
+const modalIconSeventeen = document.querySelector(".icone-17")
+modalButtonSeventeen.addEventListener("click", function(){
+    modalContentSeventeen.classList.remove("hide")
+})
+modalIconSeventeen.addEventListener("click", function(){
+    modalContentSeventeen.classList.add("hide")
+})
+
+const modalButtonEighteen = document.querySelector(".button-18")
+const modalContentEighteen = document.querySelector(".teste-18")
+const modalIconEighteen = document.querySelector(".icone-18")
+modalButtonEighteen.addEventListener("click", function(){
+    modalContentEighteen.classList.remove("hide")
+})
+modalIconEighteen.addEventListener("click", function(){
+    modalContentEighteen.classList.add("hide")
+})
+
+const modalButtonNineteen = document.querySelector(".button-19")
+const modalContentNineteen = document.querySelector(".teste-19")
+const modalIconNineteen = document.querySelector(".icone-19")
+modalButtonNineteen.addEventListener("click", function(){
+    modalContentNineteen.classList.remove("hide")
+})
+modalIconNineteen.addEventListener("click", function(){
+    modalContentNineteen.classList.add("hide")
+})
+
+const modalButtonTwenty = document.querySelector(".button-20")
+const modalContentTwenty = document.querySelector(".teste-20")
+const modalIconTwenty = document.querySelector(".icone-20")
+modalButtonTwenty.addEventListener("click", function(){
+    modalContentTwenty.classList.remove("hide")
+})
+modalIconTwenty.addEventListener("click", function(){
+    modalContentTwenty.classList.add("hide")
+})
+
+const modalButtonTwentyOne = document.querySelector(".button-21")
+const modalContentTwentyOne = document.querySelector(".teste-21")
+const modalIconTwentyOne = document.querySelector(".icone-21")
+modalButtonTwentyOne.addEventListener("click", function(){
+    modalContentTwentyOne.classList.remove("hide")
+})
+modalIconTwentyOne.addEventListener("click", function(){
+    modalContentTwentyOne.classList.add("hide")
+})
+
+const modalButtonTwentyTwo = document.querySelector(".button-22")
+const modalContentTwentyTwo = document.querySelector(".teste-22")
+const modalIconTwentyTwo = document.querySelector(".icone-22")
+modalButtonTwentyTwo.addEventListener("click", function(){
+    modalContentTwentyTwo.classList.remove("hide")
+})
+modalIconTwentyTwo.addEventListener("click", function(){
+    modalContentTwentyTwo.classList.add("hide")
+})
+
+const modalButtonTwentyThree = document.querySelector(".button-23")
+const modalContentTwentyThree = document.querySelector(".teste-23")
+const modalIconTwentyThree = document.querySelector(".icone-23")
+modalButtonTwentyThree.addEventListener("click", function(){
+    modalContentTwentyThree.classList.remove("hide")
+})
+modalIconTwentyThree.addEventListener("click", function(){
+    modalContentTwentyThree.classList.add("hide")
+})
+
+const modalButtonTwentyFour = document.querySelector(".button-24")
+const modalContentTwentyFour = document.querySelector(".teste-24")
+const modalIconTwentyFour = document.querySelector(".icone-24")
+modalButtonTwentyFour.addEventListener("click", function(){
+    modalContentTwentyFour.classList.remove("hide")
+})
+modalIconTwentyFour.addEventListener("click", function(){
+    modalContentTwentyFour.classList.add("hide")
+})
+
+const modalButtonTwentyFive = document.querySelector(".button-25")
+const modalContentTwentyFive = document.querySelector(".teste-25")
+const modalIconTwentyFive = document.querySelector(".icone-25")
+modalButtonTwentyFive.addEventListener("click", function(){
+    modalContentTwentyFive.classList.remove("hide")
+})
+modalIconTwentyFive.addEventListener("click", function(){
+    modalContentTwentyFive.classList.add("hide")
+})
+
+const modalButtonTwentySix = document.querySelector(".button-26")
+const modalContentTwentySix = document.querySelector(".teste-26")
+const modalIconTwentySix = document.querySelector(".icone-26")
+modalButtonTwentySix.addEventListener("click", function(){
+    modalContentTwentySix.classList.remove("hide")
+})
+modalIconTwentySix.addEventListener("click", function(){
+    modalContentTwentySix.classList.add("hide")
+})
+
+const modalButtonTwentySeven = document.querySelector(".button-27")
+const modalContentTwentySeven = document.querySelector(".teste-27")
+const modalIconTwentySeven = document.querySelector(".icone-27")
+modalButtonTwentySeven.addEventListener("click", function(){
+    modalContentTwentySeven.classList.remove("hide")
+})
+modalIconTwentySeven.addEventListener("click", function(){
+    modalContentTwentySeven.classList.add("hide")
+})
+
+
+
+
 
 
 
